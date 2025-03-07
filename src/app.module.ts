3e47208086f30224e49b44dffb1d67c9d6b6bcd9
ala-kad <kaddechiala@gmail.com>
2025-03-07 16:27:20 +0100

    fix!: imported application modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthzModule } from './authz/authz.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsrModule } from './usr/usr.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthzModule, 
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the ConfigModule available everywhere
+    }), 
+    UsrModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
