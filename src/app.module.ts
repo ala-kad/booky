b4b8340c3b773b50b4177ed077224bbcfccdbebf
ala-kad <kaddechiala@gmail.com>
2025-03-08 15:50:08 +0100

    feat!: GraphQLModule + TypeOrmModule configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,12 @@ import { AppService } from './app.service';
 import { AuthzModule } from './authz/authz.module';
 import { ConfigModule } from '@nestjs/config';
 import { UsrModule } from './usr/usr.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './books/entities/book.entity';
 
 @Module({
   imports: [
@@ -12,6 +18,26 @@ import { UsrModule } from './usr/usr.module';
       isGlobal: true, // Makes the ConfigModule available everywhere
     }), 
     UsrModule,
+  GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    typePaths: ['./src/**/*.graphql'],
+    definitions: {
+      path: join(process.cwd(), 'src/graphql.ts'),
+      outputAs: 'class',
+    },
+  }),
+  BooksModule,
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'booky',
+    entities: [Book],
+    synchronize: true,
+    autoLoadEntities: true,
+  }),
   ],
   controllers: [AppController],
   providers: [AppService],
