22b254033b6c359ce92f5b46b6db233a13c24352
ala-kad <kaddechiala@gmail.com>
2025-03-08 23:30:05 +0100

    fix!: fixed typeorm config to use sqlite3
    BREAKING CHANGE: refactored and cleaned code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,43 +1,45 @@
+//  Modules
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthzModule } from './authz/authz.module';
 import { ConfigModule } from '@nestjs/config';
-import { UsrModule } from './usr/usr.module';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
-import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthzModule } from './authz/authz.module';
+import { UsrModule } from './usr/usr.module';
+import { BooksModule } from './books/books.module';
+// Services
+import { AppService } from './app.service';
+// Drivers
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+// Controllers
+import { AppController } from './app.controller';
+// Utils
+import { join } from 'path';
+// Entities
 import { Book } from './books/entities/book.entity';
 
 @Module({
   imports: [
     AuthzModule, 
+    UsrModule,
+    BooksModule,
     ConfigModule.forRoot({
       isGlobal: true, // Makes the ConfigModule available everywhere
     }), 
-    UsrModule,
-  GraphQLModule.forRoot<ApolloDriverConfig>({
-    driver: ApolloDriver,
-    typePaths: ['./src/**/*.graphql'],
-    definitions: {
-      path: join(process.cwd(), 'src/graphql.ts'),
-      outputAs: 'class',
-    },
-  }),
-  BooksModule,
-  TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '',
-    database: 'booky',
-    entities: [Book],
-    synchronize: true,
-    autoLoadEntities: true,
-  }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./src/**/*.graphql'],
+      definitions: {
+       path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: join(__dirname, '..', 'booky.sqlite'), // Save the database file in the root folder'
+      entities: [Book],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
