989961942cf4666b117c42c03362056c0c9f5238
ala-kad <kaddechiala@gmail.com>
2025-03-08 15:51:31 +0100

    feat: generated graphql code-first file

---
+++ src/graphql.ts
@@ -0,0 +1,42 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export class CreateBookInput {
+    name: string;
+    description?: Nullable<string>;
+}
+
+export class UpdateBookInput {
+    id: number;
+    name?: Nullable<string>;
+    description?: Nullable<string>;
+}
+
+export class Book {
+    id: number;
+    name: string;
+    description?: Nullable<string>;
+}
+
+export abstract class IQuery {
+    abstract books(): Nullable<Book>[] | Promise<Nullable<Book>[]>;
+
+    abstract book(id: number): Nullable<Book> | Promise<Nullable<Book>>;
+}
+
+export abstract class IMutation {
+    abstract createBook(createBookInput: CreateBookInput): Book | Promise<Book>;
+
+    abstract updateBook(updateBookInput: UpdateBookInput): Book | Promise<Book>;
+
+    abstract removeBook(id: number): Nullable<Book> | Promise<Nullable<Book>>;
+}
+
+type Nullable<T> = T | null;
