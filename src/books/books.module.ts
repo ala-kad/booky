b1ccc96557865aa2cb361f115cac435d428896ad
ala-kad <kaddechiala@gmail.com>
2025-03-12 12:05:09 +0100

    feat: injected apollo service in booksModule providers

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,12 +1,16 @@
+// Modules
 import { Module } from '@nestjs/common';
-import { BooksService } from './books.service';
-import { BooksResolver } from './books.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// Services
+import { BooksService } from './books.service';
+import { ApolloService } from '../apollo/apollo.service'
+import { BooksResolver } from './books.resolver';
+// Entities
 import { Book } from './entities/book.entity';
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book])],
-  providers: [BooksResolver, BooksService]
+  providers: [BooksResolver, BooksService, ApolloService]
 })
 export class BooksModule {}
