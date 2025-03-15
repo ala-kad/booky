2554e6aea270b82e3c23b92993c2c2ed47baec70
ala-kad <kaddechiala@gmail.com>
2025-03-15 22:57:26 +0100

    fix: create-book inputs validation

--- src/books/dto/create-book.input.ts
+++ src/books/dto/create-book.input.ts
@@ -1,9 +1,13 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
 
+@InputType()
 export class CreateBookInput {
     @Field()
+    @IsNotEmpty({ message: 'Name cannot be empty' })
     name: string;
-  
-    @Field({ nullable: true })
-    description?: string;
+
+    @Field()
+    @IsNotEmpty({ message: 'Description cannot be empty' })
+    description: string;
 }
