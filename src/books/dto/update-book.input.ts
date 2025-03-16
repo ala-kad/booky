e1fe965c5f966de446c93c2261a595b5ba77a7cd
ala-kad <kaddechiala@gmail.com>
2025-03-17 00:04:52 +0100

    fix: update book input fields validation

--- src/books/dto/update-book.input.ts
+++ src/books/dto/update-book.input.ts
@@ -1,6 +1,14 @@
 import { CreateBookInput } from './create-book.input';
 import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional, IsNotEmpty } from 'class-validator';
 
 export class UpdateBookInput extends PartialType(CreateBookInput) {
+  @IsNotEmpty()
   id: number;
+
+  @IsOptional()
+  name?: string;
+
+  @IsOptional()
+  description?: string;
 }
