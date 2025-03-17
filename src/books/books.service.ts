93667230cd41b329f05182d9305260865b46eb3f
ala-kad <kaddechiala@gmail.com>
2025-03-17 23:39:27 +0100

    feat!: documented and commented code methods

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -15,21 +15,41 @@ export class BooksService {
     private booksRepository: Repository<Book>,
   ) {}
 
+  /**
+   * 
+   * @param createBookInput required argument to create a new book. It contains name and description fields that are required for mutation
+   * @returns Created Book entity with its propreties e.g Book { id: 1, name: 'My book', description: 'Book description' }
+   */
   async create(createBookInput: CreateBookInput): Promise<Book> {
     const book = this.booksRepository.create(createBookInput);
-    return this.booksRepository.save(book);
+    return await this.booksRepository.save(book);
   }
 
+  /**
+   * 
+   * @returns an array of found books entities in the DB. e.g: Book [Book] / Book[] 
+   */
   async findAll(): Promise<Book[]> {
     const books = await this.booksRepository.find();
     return books;
   }
   
+  /**
+   * 
+   * @param id filter creteria to query a particular book
+   * @returns Book entity with the matching id
+   */
   async findOne(id: number): Promise<Book> {
     const book = await this.booksRepository.findOneByOrFail({ id });
     return book;
   }
 
+  /**
+   * 
+   * @param id used as a filtering creteria to quer a particular book
+   * @param updateBookInput used as an argument to pass the update inputs values. e.g: updateBookInput:{ id:1, name: "update book name" } 
+   * @returns updated Book entity | NotFoundException Error
+   */
   async update(id: number, updateBookInput: UpdateBookInput): Promise<Book | null> {
     const book = await this.booksRepository.findOne({ where: { id } });
     if (!book) {
@@ -40,6 +60,11 @@ export class BooksService {
     return this.booksRepository.findOne({ where: { id } }); // Return updated book
   }
 
+  /**
+   * 
+   * @param id used to find a book to delete
+   * @returns Book entity with deletion status 
+   */
   async remove(id: number): Promise<Book> {
     const book = await this.booksRepository.findOneByOrFail({id});
     await this.booksRepository.remove(book);
