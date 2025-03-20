c67874981dab6c009b801b19e96448d1b6c5feb9
ala-kad <kaddechiala@gmail.com>
2025-03-20 23:46:28 +0100

    fix: bind server listen port to PORT .env variable

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule , { cors: true });
   app.useGlobalPipes(new ValidationPipe()); // ✅ Enable validation
-  await app.listen(8000);
   app.enableCors();
+  await app.listen(process.env.PORT? process.env.PORT : 8000);
 }
 bootstrap();
